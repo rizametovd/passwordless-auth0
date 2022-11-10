a0ea8a2fe4a6bd35368a9868146dbbd16b723e97
David Rizametov <rizametov@gmail.com>
2022-11-10 18:01:25 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,8 @@ import UsersPage from './pages/UsersPage/UsersPage';
 const App: React.FC = () => {
   // console.log('useAuth0:', useAuth0());
 
-  console.log('1')
-  console.log('2')
-  console.log('3')
+  console.log('a')
+
 
   const routes = useRoutes([
     {
