43e075c1f016616c13011250c6b28f50c2973492
David Rizametov <rizametov@gmail.com>
2022-11-10 17:01:24 +0300

    А сейчас сработало?

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useAuth0 } from '@auth0/auth0-react';
+// import { useAuth0 } from '@auth0/auth0-react';
 import { useRoutes } from 'react-router-dom';
 import { ProtectedRoute } from './auth/ProtectedRoute/ProtectedRoute';
 import HomePage from './pages/HomePage/HomePage';
@@ -7,6 +7,10 @@ import UsersPage from './pages/UsersPage/UsersPage';
 
 const App: React.FC = () => {
   // console.log('useAuth0:', useAuth0());
+
+  console.log('1')
+  console.log('2')
+  console.log('3')
 
   const routes = useRoutes([
     {
